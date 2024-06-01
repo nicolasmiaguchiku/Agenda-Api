d392d1396f0f402d0b81d022fc867e558e7cd87c
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2024-06-01 14:45:37 -0300

    Projeto iniciado

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkcore;
+using AgendaApi.Entities;
+using System.Data.Entity;
+
+namespace AgendaApi.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContext<AgendaContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Contatos> Contatos { get; set; }
+    }
+}
---
+++ Entities/Contatos.cs
@@ -0,0 +1,10 @@
+﻿namespace AgendaApi.Entities
+{
+    public class Contatos
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Phone{ get; set; }
+        public bool Active { get; set; }
+    }
+}
