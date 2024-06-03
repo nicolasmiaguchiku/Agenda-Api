9543f21674063aea597acd3c7b7e5fdf0fd3fcc3
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2024-06-02 21:09:20 -0300

    Instalando bibliotecas e criando controller

--- obj/Release/net8.0/AgendaApi.AssemblyInfo.cs
+++ obj/Release/net8.0/AgendaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d392d1396f0f402d0b81d022fc867e558e7cd87c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
