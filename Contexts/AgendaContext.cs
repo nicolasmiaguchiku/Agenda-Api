9c24a3903a14cf4b7504a127433c6d29f1d2822b
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2024-06-02 21:35:11 -0300

    Criacao Dbcontext e Craicao da Tabela Agenda

---
+++ Contexts/AgendaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AgendaApi.Entities;
+
+namespace AgendaApi.Contexts
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contatos> Contatos { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-
+using AgendaApi.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
